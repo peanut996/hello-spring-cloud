6813c339a4d7aa862b024f85b59d3726ac63ed3b
peanut996 <849421294godw@gmail.com>
2024-04-03 18:46:44 +0800

    autowire discovery client.

--- hello-spring-cloud-provider/src/main/java/com/example/hellospringcloud/controller/AppController.java
+++ hello-spring-cloud-provider/src/main/java/com/example/hellospringcloud/controller/AppController.java
@@ -1,9 +1,10 @@
 package com.example.hellospringcloud.controller;
 
 import com.example.hellospringcloud.annotation.MeasureExecutionTime;
-//import com.netflix.discovery.EurekaClient;
-//import com.netflix.discovery.shared.Applications;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Applications;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AppController {
 
+    private final DiscoveryClient discoveryClient;
 
-//    private final EurekaClient discoveryClient;
-//
-//    @GetMapping("/apps")
-//    @MeasureExecutionTime
-//    Applications list() {
-//        return discoveryClient.getApplications();
-//    }
+    private final EurekaClient eurekaClient;
+
+    @GetMapping("/apps")
+    @MeasureExecutionTime
+    Applications list() {
+        return eurekaClient.getApplications();
+    }
 }
